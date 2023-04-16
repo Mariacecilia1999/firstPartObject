062e439e1270f3e045b27a24103c4afd7eafc73d
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 20:15:06 -0300

    Adding slogan exercise data base

---
+++ main.js
@@ -0,0 +1,12 @@
+// Nos pasaron un listado de personas y necesitamos guardarlo en una base de datos
+// Los datos que tenemos son un id, email, nombre y teléfono
+// Crear un objeto para cada persona que nos pasaron
+// Mostrar por consola todos los objetos creados
+
+// DATOS DE LAS PERSONAS QUE NECESITAMOS GUARDAR EN UNA BASE DE DATOS
+// 1,ada@gmail.com,Ada Lovelace,1234567890
+// 2,grace@hotmail.com,Grace Hopper,0987654321
+// 3,hedy@gmail.com,Hedy Lamarr,6789054321
+// 4,radia@yahoo.com,Radia Perlman,1234509876
+// 5,Sheryl@facebook.com,Sheryl Sandberg,5432167890
+
