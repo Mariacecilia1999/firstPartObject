0241b64006fc19993ee4ac233faa9a6118c6942c
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 20:56:33 -0300

    Adding slogan exercise data correction

---
+++ main.js
@@ -0,0 +1,3 @@
+// Tenemos la variable datos que tiene un objeto con los datos de una persona
+// El teléfono está mal y tenemos que modificarlo, pero no podemos modificar la definición/creación del objeto. El télefono correcto es 0192837465
+// También tenemos que modificar la edad, ya que es un string y necesitamos que sea un número.
