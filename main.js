ecec8e33aa6550c2f64e00490467b7ffb5efac57
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 18:56:27 -0300

    Add exercise slogan song

---
+++ main.js
@@ -0,0 +1,3 @@
+// Tenemos un objeto en la variable song con diferentes propiedades que representan una canción de spotify.
+// Queremos crear un nuevo objeto, guardandolo en la variable cancion, a partir de las propiedades del objeto que tenemos en song
+// El nuevo objeto tiene que tener las propiedades titulo, banda, y duracion (en segundos, ya que en el objeto original song está en milisegundos)
