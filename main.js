517555380cf1a0bddd2fe9e27b1405b773dacec8
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:07:26 -0300

    Adding slogan exercise favorite language

---
+++ main.js
@@ -0,0 +1,4 @@
+// Tenemos la variable datos con un objeto que guarda datos de personas
+// Tenemos que programar la siguiente lógica
+// SI la propiedad programa es true
+// Agregá la propiedad lenguajeFavorito, y asignale "Javascript"
