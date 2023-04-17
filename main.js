508654a1df734093814178d6ca084c290ef7bd11
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:34:08 -0300

    Adding slogan exercise skills

---
+++ main.js
@@ -0,0 +1,3 @@
+// Tenemos la variable user, que es un objeto con datos de una persona en linkedin
+// En el objeto hay una propiedad skills, que es un array con las distintas habilidades de la persona
+// Necesitamos actualizar el perfil y agregar "Javascript" al array de de skills
