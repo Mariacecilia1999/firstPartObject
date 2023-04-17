a6e9545c8eaad15cc645d9838baa9ed598197f4e
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 23:27:20 -0300

    Adding slogan  exercise winner

---
+++ main.js
@@ -0,0 +1,4 @@
+// Tenemos un array de objetos, con las ganadoras de algunas temporadas de Rupaul.
+// Cada objeto tiene las propiedades nombre, temporada y foto.
+// Iterá/Recorré el array utilizando for y mostrá el nombre y la temporada que ganó. Por ejemplo: Bianca Del Rio ganó la temporada 6
+
