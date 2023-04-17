61bf1ebc199142b095ab50bed248e8b18f37df24
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 23:59:03 -0300

    Adding slogan exercise 'Songs'

---
+++ main.js
@@ -0,0 +1,5 @@
+// Tenemos un array de objetos, que representa un listado de bandas
+// Cada objeto representa una banda, y tiene las propidades id, nombre, fundacion, activa
+// Mostrar en consola cada banda del array, con la siguiente lógica:
+// Si la banda está activa mostrar el mensaje: "[NOMBRE DE LA BANDA] está activa desde el año [AÑO DE FUNDACIÓN]"
+// Si la banda NO está activa mostrar el mensaje: "[NOMBRE DE LA BANDA] no está activa"
