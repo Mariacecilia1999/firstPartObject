561f7c44578a8739f76e560585e1ac9cf6c618d5
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-17 00:15:04 -0300

    Adding slogan exercise 'Band'

---
+++ main.js
@@ -0,0 +1,4 @@
+// Tenemos un objeto en la variable banda, con datos de una banda (nombre, año de lanzamiento, si sigue en actividad, una foto, los integrantes y una lista de sus discos).
+// Queremos mostrar:
+// La duración total del disco (suma de la duración de cada canción)
+// La duración promedio por canción (un promedio entre todas las duraciones)
