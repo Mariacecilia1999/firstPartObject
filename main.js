b1a5dcf4c0a2bdac22755fac06a4ad6decb11fa5
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:16:32 -0300

    Addign slogan exercise

---
+++ main.js
@@ -0,0 +1,5 @@
+// Tenemos la variable datos con un objeto que guarda datos de una persona
+// Tenemos que programar la siguiente lógica
+// SI la propiedad programa es true
+// Agregá la propiedad lenguajesFavoritos, y asignale un array con dos lenguajes de programación
+// Al final, mostrar en la consola la siguiente frase: "Hola, mi nombre es [ACA EL NOMBRE] y programo en [ACA EL LISTADO DE LENGUAJES FAVORITOS]"
