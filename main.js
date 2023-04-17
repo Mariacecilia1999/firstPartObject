45f978a9916960f3ebac8d30553905a84cc5c06f
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:24:07 -0300

    Adding result exercise

--- main.js
+++ main.js
@@ -3,3 +3,26 @@
 // SI la propiedad programa es true
 // Agregá la propiedad lenguajesFavoritos, y asignale un array con dos lenguajes de programación
 // Al final, mostrar en la consola la siguiente frase: "Hola, mi nombre es [ACA EL NOMBRE] y programo en [ACA EL LISTADO DE LENGUAJES FAVORITOS]"
+
+const datos = {
+   id: 1,
+   nombre: "Ada",
+   apellido: "Lovelace",
+   email: "ada.lovelace@gmail.com",
+   telefono: "1234567890",
+   edad: 29,
+   programa: true,
+ };
+
+
+ const checkProgram = () =>{
+   if(datos.programa == true){
+      datos.lenguajesFavoritos = ['JavaScript', 'Java']
+   }
+ }
+ 
+checkProgram()
+
+ console.log(
+   `Hola, mi nombre es ${datos.nombre} y programo en ${datos.lenguajesFavoritos}`
+ );
