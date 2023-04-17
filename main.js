5bca6e998975ef206dd960b38367397ec3972ac0
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:13:35 -0300

    Adding result exercise 'Favorite language'

--- main.js
+++ main.js
@@ -2,3 +2,23 @@
 // Tenemos que programar la siguiente lógica
 // SI la propiedad programa es true
 // Agregá la propiedad lenguajeFavorito, y asignale "Javascript"
+
+var datos = {
+   id: 1,
+   nombre: "Ada",
+   apellido: "Lovelace",
+   email: "ada.lovelace@gmail.com",
+   telefono: "1234567890",
+   edad: 29,
+   programa: true,
+ };
+
+const checkProgram = () =>{
+   if(datos.programa == true){
+      datos.lenguajeFavorito = 'Javascript'
+   }
+}
+
+checkProgram()
+
+console.log(datos)
