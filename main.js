d3c6940831d7583dc5d1bd2e05f79a85c2dc3d07
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:29:03 -0300

    Adding slogan exercise 'Disc'

---
+++ main.js
@@ -0,0 +1,3 @@
+// Tenemos la variable disco con un objeto que guarda datos de un album musical
+// Queremos obtener el nombre y año de lanzamiento del disco y nombre de la banda, para mostrar el mensaje "El disco [NOMBRE DISCO] de la banda [NOMBRE DE LA BANDA] se lanzó en el año [AÑO DE LANZAMIENTO DEL DISCO]"
+
