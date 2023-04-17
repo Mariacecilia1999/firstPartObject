afb6127178e6458c5952a3b1a9a60f7cf83850db
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-16 22:01:25 -0300

    Adding result exercise 'Data correction'

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 // Tenemos la variable datos que tiene un objeto con los datos de una persona
 // El teléfono está mal y tenemos que modificarlo, pero no podemos modificar la definición/creación del objeto. El télefono correcto es 0192837465
 // También tenemos que modificar la edad, ya que es un string y necesitamos que sea un número.
+
+var datos = {
+   id: 1,
+   nombre: "Ada",
+   apellido: "Lovelace",
+   email: "ada.lovelace@gmail.com",
+   telefono: "1234567890",
+   edad: "29",
+   programa: true,
+ };
+
+ datos.edad = 29
+ datos.telefono = '0192837465'
+
+ console.log(datos)
