0fe924fc236aedc194d09dff354184df7646c6f7
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-17 00:12:28 -0300

    Adding results exercise 'Songs'

--- main.js
+++ main.js
@@ -3,3 +3,23 @@
 // Mostrar en consola cada banda del array, con la siguiente lógica:
 // Si la banda está activa mostrar el mensaje: "[NOMBRE DE LA BANDA] está activa desde el año [AÑO DE FUNDACIÓN]"
 // Si la banda NO está activa mostrar el mensaje: "[NOMBRE DE LA BANDA] no está activa"
+var bandas = [
+   { id: 1, nombre: "Nirvana", fundacion: 1987, activa: false },
+   { id: 2, nombre: "Foo Fighters", fundacion: 1994, activa: true },
+   { id: 3, nombre: "Led Zeppelin", fundacion: 1967, activa: false },
+   { id: 3, nombre: "Queens of the Stone Age", fundacion: 1997, activa: true },
+   { id: 3, nombre: "Pearl Jam", fundacion: 1990, activa: true },
+ ];
+ 
+ 
+
+const checkBand = (objectArray) =>{
+   for(const band of objectArray){
+      if(band.activa == true){
+         console.log(`${band.nombre} está activa desde el año ${band.fundacion}`)
+      }else{
+         console.log(`${band.nombre} no está activa`)
+      }
+   }
+}
+checkBand(bandas)
